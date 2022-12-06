95ac9f678ac25d01ec01bebb88462faa8fa45b68
miun-jvig <jovi1802@student.miun.se>
2022-12-06 23:47:25 +0100

    Initial commit

---
+++ app/src/main/java/miun/fl/dt142g/projektkok/MainActivity.java
@@ -0,0 +1,14 @@
+package miun.fl.dt142g.projektkok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
