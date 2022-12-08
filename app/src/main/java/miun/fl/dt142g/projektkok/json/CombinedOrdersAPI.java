4d6618b4ca25f0b2ed71297382eeed08cad54092
miun-jvig <jovi1802@student.miun.se>
2022-12-09 00:17:33 +0100

    Started working on DB connection for orders.

--- app/src/main/java/miun/fl/dt142g/projektkok/json/CombinedOrdersAPI.java
+++ app/src/main/java/miun/fl/dt142g/projektkok/json/CombinedOrdersAPI.java
@@ -5,6 +5,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface CombinedOrdersAPI {
-    @GET("orders/kitchen")
+    @GET("combinedOrders")
     Call<List<CombinedOrders>> getOrders();
 }
